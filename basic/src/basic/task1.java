34be6e55c3db0db370d4b31ddc120c59f08ba696
seteen0717 <k_settsun@yahoo.co.jp>
2019-12-18 15:07:27 +0900

    課題提出-Javaの基礎

---
+++ basic/src/basic/task1.java
@@ -0,0 +1,11 @@
+package basic;
+
+public class task1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+        System.out.println("初めての課題");
+        System.out.print("Java攻略の道");
+	}
+
+}
---
+++ basic/src/basic/task2.java
@@ -0,0 +1,11 @@
+package basic;
+
+public class task2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+        System.out.println("hogehoge");
+        System.out.println("hogehoge2");
+	}
+
+}
