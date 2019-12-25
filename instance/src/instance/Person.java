5cd65d2b6f5c1d5e34d7da9af06388caec821f69
seteen0717 <k_settsun@yahoo.co.jp>
2019-12-25 13:37:40 +0900

    課題提出ーインスタンスとクラス

---
+++ instance/src/instance/Person.java
@@ -0,0 +1,12 @@
+package instance;
+
+public class Person {
+	String name = "世戸 康介";
+	int age = 30;
+
+	public void myProfile() {
+		System.out.println("私の名前は" + this.name + "です");
+		System.out.println("年齢は" + this.age + "です");
+		System.out.println("よろしくお願いします");
+	}
+}
---
+++ instance/src/instance/task3.java
@@ -0,0 +1,12 @@
+package instance;
+
+public class task3 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Person person = new Person();
+
+		person.myProfile();
+	}
+
+}
